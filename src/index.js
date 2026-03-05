71b847e51e2a9cd7647c14fe35f992c95a974775
sujeethkoulas <168056490+sujeethkoulas@users.noreply.github.com>
2026-03-05 16:35:28 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MarketplaceApp from './marketplace_app';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MarketplaceApp />
+  </React.StrictMode>
+);
